065fbfe138e49476e45e3559c07e550063e4c9ac
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2023-02-20 18:05:35 +0530

    fix(emojis): Update Emoji Handling logic

--- src/animations/wrap.js
+++ src/animations/wrap.js
@@ -2,6 +2,7 @@ const cwd = process.cwd().split('discord-wrapped')[0] + 'discord-wrapped/src/';
 const pwd = process.cwd().split('discord-wrapped')[0] + 'discord-wrapped/';
 
 const Canvas = require('canvas');
+const sharp = require('sharp');
 
 const fs = require('fs');
 const editly = require('editly');
@@ -81,11 +82,18 @@ module.exports = async () => {
 			.map((char) => char.codePointAt(0).toString(16))
 			.join('-');
 		if(codepoint == '1f979-1f979-1f979') codepoint = '1f614';
-		// const url = `https://emoji.aranja.com/static/emoji-data/img-twitter-72/${codepoint}.png`;
-		const url = `https://emojiapi.dev/api/v1/${codepoint}/512.png`
+		
+		// request twitter api for 512x512px twemoji
+		const url = `https://abs.twimg.com/emoji/v2/svg/${codepoint}.svg`
 
 		const response = await axios.get(url, { responseType: 'arraybuffer' });
-		return response.data;
+		
+		// convert svg to 512x512px png
+		const buffer = await sharp(response.data)
+			.resize(512, 512)
+			.png()
+			.toBuffer();
+		return buffer;
 	}
 	async function fetchDiscordEmojiBuffer(id) {
 		const url = `https://cdn.discordapp.com/emojis/${id}.png`;
